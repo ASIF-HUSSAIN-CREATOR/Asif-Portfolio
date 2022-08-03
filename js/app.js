09ba1b88d24028c57bacfbdc7b2daf6e21fad9ef
ASIF-HUSSAIN-CREATOR <108573871+ASIF-HUSSAIN-CREATOR@users.noreply.github.com>
2022-08-03 17:02:04 +0530

    Add files via upload
    my first Commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.slider').slick({
+arrows:false,
+dots:true,
+appendDots:'.slider-dots',
+dotsClass:'dots'
+    });
+    
+})
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
